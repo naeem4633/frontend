adefaf0fa14be32b62b0c383e8cfb8de980ee3e6
naeem4633 <ahmedn3700@gmail.com>
2023-07-25 12:33:58 +0500

    Header and Footer completed.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,77 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full absolute bottom-0 flex flex-col'>
+        <div className='bg-black min-h-[60vh] mx-auto w-5/6 flex flex-col justify-between p-4'>
+            <div className='w-full flex flex-row text-white text-sm justify-evenly'>
+                <ul className='w-1/5 space-y-4'>
+                    <li className='flex flex-row space-x-2 items-baseline'>
+                        <img className='w-4 h-4' src='../static/images/footer-location.png'></img>
+                        <p className='font-semibold'>Find a Store:</p>
+                        <p className='text-semibold'>Store Locator</p>
+                    </li>
+                    <li className='flex flex-row space-x-2 items-baseline'>
+                        <img className='w-4 h-4' src='../static/images/footer-headphone.png'></img>
+                        <p className='font-semibold'>Call Us:</p>
+                        <p className='text-semibold'>(+92) - 304-111-0075</p>
+                    </li>
+                    <li className='flex flex-row space-x-2 items-baseline'>
+                        <img className='w-4 h-4' src='../static/images/footer-email.png'></img>
+                        <p className='font-semibold'>Email:</p>
+                        <p className='text-semibold'>support@vegas.pk</p>
+                    </li>
+                    <li className='flex flex-row space-x-2 items-baseline'>
+                        <img className='w-4 h-4' src='../static/images/footer-clock.png'></img>
+                        <p className='font-semibold'>Hours:</p>
+                        <p className='text-semibold'>09:00 - 20:00, Mon - Sat</p>
+                    </li>
+                </ul>
+                <ul className='w-1/5 space-y-4 pt-2'>
+                    <p className='text-xl font-semibold py-2'>Company</p>
+                    <li className='flex flex-row space-x-2 items-baseline'>About Us</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Contact us</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Privacy Policy</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Terms & Conditions</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Return & Exhange Policy</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Frequently Asked Question</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Blog</li>
+                </ul>
+                <ul className='w-1/5 space-y-4 pt-2'>
+                    <p className='text-xl font-semibold py-2'>Account</p>
+                    <li className='flex flex-row space-x-2 items-baseline'>Sign In</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>View Cart</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>My Wishlist</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Track My Order</li>
+                </ul>
+                <ul className='w-1/5 space-y-4 pt-2'>
+                    <p className='text-xl font-semibold py-2'>Popular Brands</p>
+                    <li className='flex flex-row space-x-2 items-baseline'>NYX Professional Makeup</li>
+                    <li className='flex flex-row space-x-2 items-baseline'>Maybelline New York</li>
+                </ul>
+                <div className='flex flex-col space-y-2 w-1/5 text-base'>
+                    <p>Secured Payment Gateways</p>
+                    <div className='flex flex-row items-center space-x-4'>
+                        <img src='../static/images/footer-visa.png' className='w-16 h-16'></img>
+                        <img src='../static/images/footer-mastercard.png' className='w-16 h-10'></img>
+                    </div>
+                </div>
+            </div>
+            <div className='border border-x-0 border-b-0 border-white pt-4 flex flex-row items-center justify-between text-white'>
+                <div className='w-1/3 flex flex-row space-x-3'>
+                    <img className='w-6 h-6' src='../static/images/footer-facebook.png'></img>
+                    <img className='w-6 h-6' src='../static/images/footer-youtube.png'></img>
+                    <img className='w-6 h-6' src='../static/images/footer-instagram.png'></img>
+                </div>
+                <div className='w-1/3 flex flex-row space-x-1 items-center'>
+                    <p>© 2022 - 2023. All rights reserved.</p>
+                </div>
+                <div className='w-1/5 flex flex-col space-x-1 justify-start items-center'>
+                    <p className='text-xl text-red-600'>+76 583 1110327</p>
+                    <p className='text-sm text-gray-500'>Working Hours: 09:00 - 12:00</p>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,81 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full absolute top-0'>
+        <div className='mx-auto w-5/6 flex flex-row justify-between py-2'>
+            <div className='w-3/5 flex flex-row justify-between'>
+                <img className='w-44 border border-black'></img>
+                <div className='flex flex-row items-center w-3/4'>
+                    <input className='h-14 p-4 w-full border-2 border-red-300 text-gray-500 rounded tracking-wide' placeholder='Search for items...'></input>
+                    <img src='../static/images/search.png' className='w-5 h-5 -translate-x-24'></img>
+                </div>
+            </div>
+            <div className='flex flex-row items-center space-x-3'>
+                <img src='../static/images/phone.png' className='w-10 h-10'></img>
+                <div className='flex flex-col'>
+                    <p className='text-xl text-red-600'>+76 583 1110327</p>
+                    <p className='text-sm text-gray-500 tracking-wide'>Support Hours 09:00 - 20:00</p>
+                </div>
+            </div>
+        </div>
+        <div className='w-full border border-gray-200'>
+            <div className='mx-auto w-5/6 flex flex-row justify-between py-3'>
+                <div className='flex flex-row space-x-10 items-center'>
+                    <div className='h-12 w-44 flex flex-row color-secondary rounded items-center justify-evenly p-2'>
+                        <img src='../static/images/menu.png' className='w-5 h-5'></img>
+                        <p className='text-white'>Top Brands</p>
+                        <img src='../static/images/down-arrow-white.png' className='w-5 h-5'></img>
+                    </div>
+                    <ul className='flex flex-row space-x-6'>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Makeup</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Fashion</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Skin Care</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Hair Care</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Fragrance</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Lingerie</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                        <li className='flex flex-row items-center space-x-2'>
+                            <p>Watches</p>
+                            <img src='../static/images/down-arrow.png' className='w-2 h-2'></img>
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex flex-row space-x-10 items-center tracking-wide text-base'>
+                    <ul className='flex flex-row space-x-6'>
+                        <li className='flex flex-row items-center space-x-1'>
+                            <img src='../static/images/love.png' className='w-7 h-7'></img>
+                            <p className='text-gray-500'>Wishlist</p>
+                        </li>
+                        <li className='flex flex-row items-center space-x-1'>
+                            <img src='../static/images/bag.png' className='w-7 h-7'></img>
+                            <p className='text-gray-500'>Cart</p>
+                        </li>
+                        <li className='flex flex-row items-center space-x-1'>
+                            <img src='../static/images/user.png' className='w-7 h-7'></img>
+                            <p className='text-gray-500'>Account</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
